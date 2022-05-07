28e81bbb722645b8d6f286f9267466ecc41a82cb
ysaad01 <ysaad2325@gmail.com>
2022-05-07 13:22:13 -0400

    added get all products api route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name']
+        }
+      ]
+    })
+    res.status(200).json(productData);
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
