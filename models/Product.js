30b3c78bfb3a882123ddc87667bc3bae66c2fbed
ysaad01 <ysaad2325@gmail.com>
2022-05-07 11:52:31 -0400

    corrected type to clear error

--- models/Product.js
+++ models/Product.js
@@ -36,7 +36,7 @@ Product.init(
       }
     },
     category_id: {
-      types: DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'category',
         key: 'id'
